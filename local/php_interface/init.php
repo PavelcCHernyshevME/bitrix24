ab2dbf6f44feddbbddabc841ab335a3f253c3a54
Pavel.Chernyshev <Pavel.Chernyshev@miningelement.com>
2019-06-06 15:42:20 +0300

    dump function

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4,3 +4,9 @@ if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/eventh
     include $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/eventhandlers.php';
 }
 
+function dump($data) {
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';
+}
+
