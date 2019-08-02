54555a9af87ffa5cc21462b2511b538ee48e2650
Pavel.Chernyshev <Pavel.Chernyshev@miningelement.com>
2019-08-02 18:05:57 +0300

    4.4 Задачи. Работа со списанным временем
    Создать файл task4.php
    
    Всем подзадачам, у которых есть чек-боксы в статусе «не выполнен» необходимо добавить 3 отчета со случайным временем. Комментарий в отчете должен соответствовать названию элемента списка.

---
+++ task-editor/task4.php
@@ -0,0 +1,38 @@
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+/**
+ * Всем подзадачам, у которых есть чек-боксы в статусе «не выполнен»
+ * необходимо добавить 3 отчета со случайным временем.
+ * Комментарий в отчете должен соответствовать названию элемента списка.
+*/
+try {
+    \Bitrix\Main\Loader::includeModule('tasks');
+    $tastsDbRes = CTasks::GetList([], ['>PARENT_ID' => 0]);
+    while ($task = $tastsDbRes->GetNext()) {
+        $checkLDBRes = \Bitrix\Tasks\Internals\Task\CheckListTable::getList([
+                'filter' => ['TASK_ID' => $task['ID']],
+                'select' => ['IS_COMPLETE', 'TITLE']]
+        );
+        while ($checkListItem = $checkLDBRes->fetch()) {
+            $taskMemb = new CTaskMembers();
+            if ($checkListItem['IS_COMPLETE'] == 'N') {
+                dump($task['ID']);
+                $oTask = CTaskItem::getInstance($task['ID'], $task['CREATED_BY']);
+                for ($i = 0; $i < 3; $i++) {
+                    CTaskElapsedItem::add($oTask, [
+                        'TASK_ID' => $task['ID'],
+                        'USER_ID' => $task['CREATED_BY'],
+                        'COMMENT_TEXT' => $checkListItem['TITLE'],
+                        'MINUTES' => rand(1, 3600)
+                    ]);
+                }
+                break;
+            }
+        }
+    }
+} catch (\Throwable $exception) {
+    dump($exception->getMessage());
+    dump($exception->getTraceAsString());
+}
+
+
+?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
